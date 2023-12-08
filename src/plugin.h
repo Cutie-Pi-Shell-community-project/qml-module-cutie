6361f3578ee303aecd69a5e299adcbebf4d00eff
Erik Inkinen <erik.inkinen@erikinkinen.fi>
2023-12-08 13:35:59 +0200

    treewide: Migrate to libcutienotification

--- src/plugin.h
+++ src/plugin.h
@@ -7,9 +7,9 @@
 #include <atmosphere.h>
 #include <cutiestore.h>
 #include <cutiemodem.h>
+#include <cutienotification.h>
 
 #include "wifi/wifisettings.h"
-#include "notifications/notifications.h"
 #include "volume/cutievolume.h"
 #include "mobilenetwork/mobilenetwork.h"
 
