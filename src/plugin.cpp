f7830e201b10165d134721f2f836080b591cb1a7
Erik Inkinen <erik.inkinen@erikinkinen.fi>
2024-07-09 19:10:28 +0300

    treewide: Remove cutiefeedback
    cutiefeedback is now in libcutiefeedback which also contains its own QML
    module "Cutie.Feedback".

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -2,8 +2,6 @@
 
 void CutiePlugin::registerTypes(const char *uri)
 {
-	qmlRegisterSingletonType<Feedback>(uri, 1, 0, "CutieFeedback",
-					      &Feedback::provider);
 	qmlRegisterSingletonType<AtmosphereModel>(uri, 1, 0, "Atmosphere",
 						  &AtmosphereModel::provider);
 }
